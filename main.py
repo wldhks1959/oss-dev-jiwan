88412d42c40697c93ce9340847f9f6cfa556e01c
wldhks1959 <69555890+wldhks1959@users.noreply.github.com>
2023-03-19 21:45:12 +0900

    baekjoon 3003 solution
    When adding the key value to the list after receiving it from the dictionary, the key value received from the dictionary must be listed and then added to the new list so that no error occurs.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import sys
-N=int(input())
-a=[]
-count=0
-
-a=map(int,sys.stdin.readline().split())
-v=int(input())
-a=list(a)
-
-for i in range(0,N):
-    if(a[i]==v):
-        count+=1
-print(count)
+#동혁이가 발견한 흰색 피스의 개수
+# king,queen,rook,knight,bishop,pawn
+piece=[]
+p_piece=[]
+A={'king' : 1 ,
+   'queen' : 1 ,
+   'rook' : 2 ,
+   'knight' : 2 ,
+   'bishop' : 2 ,
+   'pawn' : 8 ,
+   }
+A_values=list(A.values())
+piece=(list(map(int,input().split())))
+for i in range(6):
+   if(piece[i]==A_values[i]):
+      p_piece.append(0)
+   else :
+      p_piece.append(A_values[i]-piece[i])
+for i in range(6):
+   print(p_piece[i], end=" ")
