f2cd1d3902dd40a94080f9f7077c375ee979b925
wldhks1959 <69555890+wldhks1959@users.noreply.github.com>
2023-03-30 23:59:11 +0900

    baekjoon 2447 solution
    It was solved using recursive and segmentation conquest algorithms.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-chess=[]
-chess=[input() for _ in range(8)] # 8열로 8행을 만든다.
-w_cnt=0
+def star(a):
+    if a == 1:
+        return ['*']
 
-for a in range(len(chess)):
-    for b in  range(len(chess)):
-        if a==0 or a%2==0: # 짝수열
-            if b%2==0: # 짝수행
-                if chess[a][b] != '.':
-                    w_cnt += 1
-        else: #홀수열
-            if b!=0 and b%2==1: #홀수행
-                if chess[a][b] != '.':
-                    w_cnt += 1
-print(w_cnt)
+    Stars = star(a // 3)
+    list = []
+
+    for S in Stars:
+        list.append(S * 3)
+    for S in Stars:
+        list.append(S + ' ' * (a // 3) + S)
+    for S in Stars:
+        list.append(S * 3)
+    return list
+
+
+n = int(input())
+print('\n'.join(star(n)))
